7ec74c2cd1b337fe92ba92f587336f0b23a876d3
Jaanhavi Sharma <as8279231@gmail.com>
2026-09-25 00:36:12 +0530

    Implemented repository scanner service

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
+
 from app.models.repository import RepositoryRequest
+from app.models.scan import ScanRequest
+
 from app.services.git_service import clone_repository
+from app.services.scanner_service import scan_repository
 
 router = APIRouter()
 
@@ -9,5 +13,13 @@ router = APIRouter()
 def register_repository(repository: RepositoryRequest):
 
     result = clone_repository(repository.github_url)
+
+    return result
+
+
+@router.post("/repository/scan")
+def scan_repository_endpoint(request: ScanRequest):
+
+    result = scan_repository(request.repository_path)
 
     return result
--- backend/app/services/scanner_service.py
+++ backend/app/services/scanner_service.py
@@ -15,22 +15,38 @@ def scan_repository(repository_path: str):
     repository = Path(repository_path)
 
     files = []
+    extension_count = {}
 
     for file in repository.rglob("*"):
 
+        # Skip folders
         if file.is_dir():
             continue
 
+        # Skip ignored folders
         if any(folder in file.parts for folder in IGNORE_FOLDERS):
             continue
 
-        files.append(
-    {
-        "name": file.name,
-        "path": str(file.relative_to(repository)),
-        "extension": file.suffix,
-        "size": file.stat().st_size
-    }
-)
+        # Count file extensions
+        extension = file.suffix.lower()
 
-    return sorted(files)
+        if extension in extension_count:
+            extension_count[extension] += 1
+        else:
+            extension_count[extension] = 1
+
+        # Store file information
+        files.append(
+            {
+                "name": file.name,
+                "path": str(file.relative_to(repository)),
+                "extension": file.suffix,
+                "size": file.stat().st_size
+            }
+        )
+
+    return {
+        "total_files": len(files),
+        "extension_summary": extension_count,
+        "files": files
+    }
